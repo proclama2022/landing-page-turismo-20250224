3c7dd694ec8ca9bb056c87f891f1e3fd7a7ca994
Your Name <info@partitaiva.it>
2025-03-21 15:48:18 +0100

    feat: aggiunto Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,19 @@ export default function RootLayout({
   return (
     <html lang="it" className="h-full">
       <head>
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qrqews2svr");
+            `
+          }}
+        />
+
         {/* Google Analytics tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-GCHXJ7JPEH"></script>
         <script
